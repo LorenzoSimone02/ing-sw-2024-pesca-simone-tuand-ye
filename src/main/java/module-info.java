622dce90baed5c78137e7365d2505f9710cecbf6
Mario Pesca <mariopesca02@gmail.com>
2024-03-18 12:53:49 +0100

    Added Gson in module-info.java
    Signed-off-by: Mario Pesca <mariopesca02@gmail.com>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module it.polimi.ingsw.progetto {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires com.google.gson;
 
     exports it.polimi.ingsw.client;
     opens it.polimi.ingsw.client to javafx.fxml;
