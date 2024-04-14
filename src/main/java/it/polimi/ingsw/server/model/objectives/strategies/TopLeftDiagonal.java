c7f73cd09f85e7b6907e24cf457137c6b52af90f
salvatoretuand <salvatore.tuand@mail.polimi.it>
2024-04-15 00:10:14 +0200

    Aggiunti algoritmi grezzi per calcolo punteggi nelle strategy (mancano object e resource strategy) N.b. Gli algoritmi sono ancora da ottimizzare

--- src/main/java/it/polimi/ingsw/server/model/objectives/strategies/TopLeftDiagonal.java
+++ src/main/java/it/polimi/ingsw/server/model/objectives/strategies/TopLeftDiagonal.java
@@ -1,16 +1,27 @@
 package it.polimi.ingsw.server.model.objectives.strategies;
 
 import it.polimi.ingsw.server.model.card.Card;
+import it.polimi.ingsw.server.model.card.CardColorEnum;
 import it.polimi.ingsw.server.model.objectives.ObjectiveStrategy;
 import it.polimi.ingsw.server.model.resources.ResourceTypeEnum;
 
 public class TopLeftDiagonal implements ObjectiveStrategy {
 
-    private ResourceTypeEnum resourceRequirment;
+    private CardColorEnum CardsColor;
     int pointsPerPattern;
 
     public int calculatePoints(Card[][] cards) {
-        return 0;
+        int points = 0;
+        for (int i = 0; i < 81 - 3; i++) {
+            for(int j = 0; j < 81 - 3; j++) {
+                if( cards[i][j].getColor() == CardsColor &&
+                    cards[i+1][j+1].getColor() == CardsColor &&
+                    cards[i+2][j+2].getColor() == CardsColor) {
+                    points = points + pointsPerPattern;
+                }
+            }
+        }
+        return points;
     }
 
 }
--- src/main/java/it/polimi/ingsw/server/model/objectives/strategies/TopRightLShape.java
+++ src/main/java/it/polimi/ingsw/server/model/objectives/strategies/TopRightLShape.java
@@ -1,16 +1,28 @@
 package it.polimi.ingsw.server.model.objectives.strategies;
 
 import it.polimi.ingsw.server.model.card.Card;
+import it.polimi.ingsw.server.model.card.CardColorEnum;
 import it.polimi.ingsw.server.model.objectives.ObjectiveStrategy;
 import it.polimi.ingsw.server.model.resources.ResourceTypeEnum;
 
 public class TopRightLShape implements ObjectiveStrategy {
 
-    private ResourceTypeEnum resourceRequirment;
+    private CardColorEnum columnCardsColor;
+    private CardColorEnum diagonalCardColor;
     int pointsPerPattern;
 
     public int calculatePoints(Card[][] cards) {
-        return 0;
+        int points = 0;
+        for (int i = 0; i < 81 - 4; i++) {
+            for(int j = 1; j < 81; j++) {
+                if( cards[i][j].getColor() == diagonalCardColor &&
+                    cards[i+1][j-1].getColor() == columnCardsColor &&
+                    cards[i+3][j-1].getColor() == columnCardsColor) {
+                    points = points + pointsPerPattern;
+                }
+            }
+        }
+        return points;
     }
 }
 
